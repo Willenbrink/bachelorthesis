fff40bb9905208f842b893e74c38228ac08c3193
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2020-11-23 17:22:44 +0100

    Finish unification/matching, clean up test.thy

--- src/test.thy
+++ src/test.thy
@@ -10,47 +10,44 @@ setup "type_pat_setup"
 ML_val \<open>@{term_pat "Suc (?x::nat)"}\<close>
 ML_val \<open>@{typ_pat "int \<Rightarrow> int"}\<close>
 
-ML_val \<open>@{term_pat "(\<lambda>x. x 1) f"}\<close>
-ML_val \<open>@{term_pat "(f x) y"}\<close>
+ML \<open>
+exception TEST
+val eq = Term.aconv_untyped
+(* Non-standard foldl for better waterfall/pipelining
+   E.g. replacing "a |> f b1 |> f b2" with "foldl f a b" or "b |> foldl f a" *)
+fun foldl f acc args = Library.foldl (fn (a,b) => f b a) (acc,args)
+fun ins t tree = Path.insert_term eq (t,t) tree
+fun del t tree = Path.delete_term eq (t,t) tree
+val terms =
+@{term "f"} ::
+@{term "f x"} ::
+@{term "f x y"} ::
+@{term "f (g x)"} ::
+@{term "f (g x) y"} ::
+@{term "f (h x)"} ::
+@{term "f (g x y z)"} ::
+@{term "f (g y y z)"} ::
+@{term "f (g (h a) y z)"} ::
+[]
 
-ML "val eq = Term.aconv_untyped"
-ML "fun dup t = (t,t)"
-ML "fun ins t tree = Path.insert_term eq (t,t) tree"
-ML_val \<open>Net.insert_term eq (@{term "f x"},@{term "f x"}) Net.empty\<close>
-ML \<open>val ex = Path.empty
- |> ins @{term "f x"}
- |> ins @{term "f (g x)"}
- |> ins @{term "f x y"}
- |> ins @{term "f (g x y)"}
- |> ins @{term "f (g x y z)"}
- |> ins @{term "f (g y y z)"}
- |> ins @{term "f (g (h a) y z)"}
- |> ins @{term "f"}
+val (pathl as Path.Node (con,rest)) = foldl ins Path.empty terms
+fun match unif tree term = Path.match unif tree term |> map (Syntax.pretty_term @{context})
 \<close>
 
-ML_val \<open>ins @{term "f"} ex handle Path.INSERT => Path.empty\<close> (* Duplicate *)
-ML \<open>val ex2 =
-     ex
-  |> Path.delete_term eq (dup @{term "f x"})
-\<close>
-(* TODO == for trees?
-*)
-ML_val \<open>ins @{term "f x"} ex2\<close>
+ML_val \<open> (* Duplicate *)
+let val _ = ins @{term "f"} pathl in raise TEST end handle Path.INSERT => "Success"\<close>
+(* TODO equality type for net? table is insert-order dependent *)
+ML_val \<open>pathl |> del @{term "f x"} |> ins @{term "f x "}\<close>
 
 ML \<open>val ks = Path.key_of_term @{term "f (g x y)"}\<close>
-ML \<open>val (Path.Node (con,tree)) = ex\<close>
-ML \<open>val test = Path.SItab.lookup tree ("f",0)\<close>
-ML_val \<open>Path.SItab.dest tree\<close>
-ML_val \<open>Path.lookup ex ks\<close>
-ML_val \<open>Path.lookup_one ex (ks |> rev |> hd)\<close>
 
-ML \<open>val t = @{term "f x"}\<close>
+ML \<open>val t = @{term_pat "?f ?g y"}\<close>
 ML \<open>val (tree as Path.Node (con,tree')) = Path.empty |> ins t\<close>
 ML_val \<open>
-        Path.match_term tree t
-\<close>
-ML_val \<open>
-        Path.SItab.lookup tree' ("f",0)
+match true tree @{term "f x"};
+match false pathl @{term "f (g x)"};
+match true pathl t;
+
 \<close>
 
 ML_val "head_of"
