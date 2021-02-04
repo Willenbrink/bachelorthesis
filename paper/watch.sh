6d420fcda10458d5819c292dd1e7293aa56bfb16
Sebastian Willenbrink <sewi@linux.fritz.box>
2021-02-04 12:14:24 +0100

    Add watch script

---
+++ paper/watch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+inotifywait -m -r --include "\.tex" -e modify . |
+    while read path action file # Unnecessary at this time
+          pdflatex -halt-on-error main.tex
+    end
