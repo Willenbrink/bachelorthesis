9a2d7c08671520e8438c4705f113b08fd5f8fcdd
Sebastian Willenbrink <sewi@linux.fritz.box>
2021-03-15 18:53:11 +0100

    WIP Preliminaries, exp. with macros

--- thesis/watch.sh
+++ thesis/watch.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env fish
+bibtex main
 pdflatex -halt-on-error main.tex
 okular ./main.pdf &
 inotifywait -m -r --include "\.tex" -e modify . |
     while read path action file # Unnecessary at this time
+	  bibtex main
           pdflatex -halt-on-error main.tex
     end
